1f4034a35ef392b124c69ffaf0ca58615ad0af59
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-03-27 10:02:57 -0700

    commit tree 2e49cb8ea0186fec65acce8f09c267292b982c6f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_accessor :sideA, :sideB, :sideC, :kind
+
+  def initialize(sideA, sideB, sideC)
+    @sideA = sideA
+    @sideB = sideB
+    @sideC = sideC
+  end
+
+  def kind 
+    if sideA + sideB <= sideC || sideB + sideC <= sideA || sideA + sideC <= sideB || sideA * sideB * sideC == 0
+        begin
+          raise TriangleError
+        end
+    elsif @sideA == @sideB && @sideB == @sideC
+      self.kind = :equilateral
+    elsif sideA == sideB || sideB == sideC || sideA == sideC
+      self.kind = :isosceles
+    else
+      self.kind = :scalene
+    end
+
+  end
+
+
+  class TriangleError < StandardError
+    def message
+      puts "not a triangle"
+    end
+    
+  end
 end
